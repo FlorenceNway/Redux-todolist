c7e8916060227acdad44e3b6024319f987878245
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-27 23:58:32 +0100

    render TodoList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import TodoList from './components/TodoList/TodoList'
 import './App.css';
 
 const App = () => {
-  const todos = useSelector(state => state.todos)
-  console.log('todos', todos)
-
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <TodoList />
     </div>
   );
 }
