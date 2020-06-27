e10731681740525a6f1d61baf4f1253ab91741f2
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-27 23:59:11 +0100

    render list items

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import './App.css';
+
+const TodoList = () => {
+  const todos = useSelector(state => state.todos)
+  console.log('todos', todos)
+
+  return (
+    <ul>
+      {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
