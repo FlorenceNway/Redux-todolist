86696607a699f6fbbfb077191f6f25b039c7a6db
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-28 23:55:11 +0100

    import TodoInput

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import TodoList from './components/TodoList/TodoList'
+import TodoList from './components/TodoList/TodoList';
+import TodoInput from './components/TodoList/TodoInput';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <TodoInput/>
       <TodoList />
     </div>
   );
