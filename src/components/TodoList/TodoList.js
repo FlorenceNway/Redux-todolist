07d849f94fee818f6616b9e1d525cf2beac52bb2
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-29 23:44:13 +0100

    added delete button

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,10 +5,17 @@ const TodoList = () => {
   const todos = useSelector(state => state.todos)
   console.log('todos', todos)
 
+    const deleteHandler = () => {
+        
+    }
+
   return (
     <ul>
       {todos.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+              <span>{todo.text}</span>
+              <button onClick={deleteHandler}>Remove</button>
+          </li>
       ))}
     </ul>
   );
