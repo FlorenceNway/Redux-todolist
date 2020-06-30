007b37ecc18f3120a950adf27c47ae062c4802cb
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-30 23:38:36 +0100

    removeTodo action

--- src/store/todos.actions.js
+++ src/store/todos.actions.js
@@ -2,5 +2,10 @@ let id = 1;
 
 export const addTodo = (payload) => ({
     type: "ADD_ToDo",
-    payload: {id: id++, payload }
+    payload: {id: id++, text: payload }
+});
+
+export const removeTodo = (id) => ({
+    type: "REMOVE_ToDo",
+    payload: id
 });
