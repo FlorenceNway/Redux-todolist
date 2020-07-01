2dec9de12188b18eb56b73a0c4d7b146734f0916
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-01 22:20:16 +0100

    complete todo type

--- src/store/todos.reducer.js
+++ src/store/todos.reducer.js
@@ -10,6 +10,12 @@ const todos = (state = initialState, {type,payload}) => {
         case 'REMOVE_TODO':
             const filterdToDos = state.todos.filter(todo => todo.id !== payload)
             return {...state, todos: [...filterdToDos]};
+        case 'COMPLETE_TODO':
+            const localTodos = [...state.todos]    
+            const todoIndex = localTodos.findIndex(localTodo => localTodo.id == payload)
+            localTodos[todoIndex].complete = !localTodos[todoIndex].complete
+            return {...state, todos: [...localTodos]}
+
         default:
             return state;
     }
