f8764b836a5492cd0c18b3769ef69ecd8724b6ce
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-01 22:19:46 +0100

    dispatch completeTodo function

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {removeTodo} from '../../store/todos.actions';
+import {completeTodo} from '../../store/todos.actions';
+import './TodoList.css';
 
 
 const TodoList = () => {
@@ -12,11 +14,15 @@ const TodoList = () => {
         dispatch(removeTodo(id))
     }
 
+    const completeHandler = (id) => {
+        dispatch(completeTodo(id))
+    }
+
   return (
     <ul>
       {todos.map(todo => (
           <li key={todo.id}>
-              <input type='checkbox'/>
+              <input type='checkbox' onChange={() => completeHandler(todo.id)}/>
               <span>{todo.text}</span>
               <button onClick={() => deleteHandler(todo.id)}>Remove</button>
           </li>
